9fb1aede00f1172357b9fb187624c29bac821d89
Viktoria Lazor <viktoria.lazor@gmail.com>
2022-08-19 00:26:49 +0300

    homework 23 calculator nodejs

---
+++ 23-calculator/nodejs/index.js
@@ -0,0 +1,6 @@
+const calculator = require('./src/calculator.js');
+
+console.log(calculator.add(10, 2));
+console.log(calculator.sub(12, 5));
+console.log(calculator.mult(7, 4));
+console.log(calculator.div(28, 2));
